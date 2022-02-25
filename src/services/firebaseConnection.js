230a6f6fb000e2aba331d96ce5207fcc7124982b
Lucio Henrique Lourenço <lucio.henrique@take.net>
2022-02-25 09:09:31 -0300

    Add Project

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBEeYxHyFlLndwHgSwZLAT4yCk4aVdAgbk",
+    authDomain: "sistemadechamados-1ab79.firebaseapp.com",
+    projectId: "sistemadechamados-1ab79",
+    storageBucket: "sistemadechamados-1ab79.appspot.com",
+    messagingSenderId: "525673951226",
+    appId: "1:525673951226:web:f16e6348b6adb6cef9be43",
+    measurementId: "G-EBN767X2KM"
+};
+
+// Initialize Firebase
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
