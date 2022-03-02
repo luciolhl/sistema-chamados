1b67e56660d1364e35de074fd8de26b8a4d518d6
Lucio Henrique <luciolhl@hotmail.com>
2022-03-01 21:50:32 -0300

    Aula 90

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
+
 import logo from '../../assets/logo.png';
 
 function SignUp() {
@@ -7,9 +9,14 @@ function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { signUp } = useContext(AuthContext);
+
   function handleSubmit(e){
     e.preventDefault();
-    alert('Clicou')
+    
+    if(nome !== '' && email !== '' && password !== ''){
+      signUp(email, password, nome)
+    }
   }
 
   return (
--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore'
 
 let firebaseConfig = {
     apiKey: "AIzaSyBEeYxHyFlLndwHgSwZLAT4yCk4aVdAgbk",
@@ -16,4 +17,4 @@ if(!firebase.apps.length){
     firebase.initializeApp(firebaseConfig);
 }
 
-export default firebase;
+export default firebase;;
