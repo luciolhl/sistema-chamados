c84c810c65ad8c7444ee7897a615f25d7eacb6a7
Lucio Henrique <luciolhl@hotmail.com>
2022-03-01 22:01:53 -0300

    Aula 91

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import { AuthContext } from '../../contexts/auth';
 import './sigin.css';
 import logo from '../../assets/logo.png';
 
@@ -7,9 +9,14 @@ function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { signIn, loadingAuth } = useContext(AuthContext);
+
   function handleSubmit(e){
     e.preventDefault();
-    alert('Clicou')
+
+    if(email !== '' && password !== ''){
+      signIn(email, password)
+    }
   }
 
   return (
@@ -22,7 +29,7 @@ function SignIn() {
             <h1>Entrar</h1>
             <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
             <input type="password" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} /> 
-            <button type="submit">Acessar</button>
+            <button type="submit">{loadingAuth ? 'Carregando...' : 'Acessar'}</button>
           </form>
 
           <Link to="/register">Criar uma conta</Link>
